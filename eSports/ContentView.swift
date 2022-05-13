3c5b826902425ed4f12c4c5ea7e01215c5d19567
Antonio Monteiro <antonio.monteiro@bancointer.com.br>
2022-05-13 11:20:48 -0300

    Initial Commit

---
+++ eSports/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eSports
+//
+//  Created by Antonio Henrique Braga Monteiro  on 13/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eSports/eSportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  eSportsApp.swift
+//  eSports
+//
+//  Created by Antonio Henrique Braga Monteiro  on 13/05/22.
+//
+
+import SwiftUI
+
+@main
+struct eSportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
